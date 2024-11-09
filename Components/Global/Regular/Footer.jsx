90395138c0d3363e6b07949fa87c3db3e834b03b
Thebinary110 <shivaanroy614@gmail.com>
2024-11-10 04:59:34 +0530

    F-B all HTF HLI

---
+++ Components/Global/Regular/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        <p>
+          Copyright © Designed &amp; Developed by
+          <a href="https://www.Shhaurya jaiswal.com/" target="_blank">
+            @Shhaurya jaiswal
+          </a>
+          2023
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
